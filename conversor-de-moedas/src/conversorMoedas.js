4c0bf1e21eec66283f71cf6a8569048ef4c91769
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-07-19 13:52:15 +0000

    Criando variáveis de ambiente + GET para o fixer.io + Retorno da API com o resultado

--- conversor-de-moedas/src/conversorMoedas.js
+++ conversor-de-moedas/src/conversorMoedas.js
@@ -4,8 +4,11 @@ import { Jumbotron, Button, Form, Col, Spinner, FormGroup, Alert, Modal } from '
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 import ListarMoedas from './listarMoedas';
+import axios from 'axios';
 
 function ConversorMoedas() {
+
+  const FIXER_IO_URL = `http://data.fixer.io/api/latest?access_key=${process.env.REACT_APP_API_KEY}`;
 
   const [ valor, setValor ] = useState('1');
   const [ moedaDe, setMoedaDe ] = useState('USD');
@@ -39,8 +42,27 @@ function ConversorMoedas() {
     event.preventDefault();
     setFormValidado(true);
     if (event.currentTarget.checkValidity() === true ){
-      setExibirModal(true);
+      setExibirSpinner(true);
+      axios.get(FIXER_IO_URL)
+        .then(res => {
+          const cotacao = obterCotacao(res.data);
+          setResultadoConversao(`${valor} ${moedaDe} = ${cotacao} ${moedaPara}`);
+          setExibirModal(true);
+          setExibirSpinner(false);
+        });
     }
+  }
+
+  function obterCotacao(data){
+    if (!data || data.success !== true){
+      return false;
+    }
+
+    const cotacaoDe = data.rates[moedaDe];
+    const cotacaoPara = data.rates[moedaPara];
+
+    const cotacao = (1 / cotacaoDe * cotacaoPara) * valor;
+    return cotacao.toFixed(2);
   }
 
   return (
@@ -89,7 +111,7 @@ function ConversorMoedas() {
             <Col sm="2">
               <Button variant="success" type="submit">
                 <span className = {exibirSpinner ? null : 'hidden'}>
-                  <Spinner animation="border" size="sm" style={ {marginRight: "10px"} }/>
+                  <Spinner animation="border" size="sm"/>
                 </span>
                 <span className = {exibirSpinner ? 'hidden' : null}>
                   Converter
