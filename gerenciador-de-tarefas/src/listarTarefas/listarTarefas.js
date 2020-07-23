70659105deb7e60128d96a1dd0655d0da4e13a0e
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-07-23 22:27:57 +0000

    Iniciando o useEffect na tela de Listagem

--- gerenciador-de-tarefas/src/listarTarefas/listarTarefas.js
+++ gerenciador-de-tarefas/src/listarTarefas/listarTarefas.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { A } from 'hookrouter';
+import { Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function ListarTarefas(){
+
+    const [tarefas, setTarefas] = useState([]);
+    const [carregarTarefas, setCarregarTarefas] = useState(true);
+
+    useEffect(() => {
+        function obterTarefas(){
+            const tarefasDB = localStorage['tarefas'];
+            let listarTarefas = tarefasDB ? JSON.parse(tarefasDB) : [];
+            setTarefas(listarTarefas);
+            console.log(listarTarefas);
+        }
+
+        if (carregarTarefas){
+            obterTarefas();
+            setCarregarTarefas(false);
+        }
+    }, [carregarTarefas]);
+
     return (
-        <A
-            href='/cadastrar'
-            className='btn btn-success btn-sm'>
-            Nova Tarefa
-        </A>
+        <div className='text-center'>
+            <h3>Tarefas a fazer</h3>
+
+            <Table striped bordered hover responsive data-testid="tabela">
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>
+                            <A href="/cadastrar"
+                                className="btn btn-success btn-sm"
+                                data-testid="btn-nova-tarefa">
+                                <FontAwesomeIcon icon={faPlus} />
+                                &nbsp;
+                                Nova Tarefa
+                            </A>
+                        </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                </tbody>
+            </Table>
+
+        </div>
     );
 }
 
