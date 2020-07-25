a18fe0d03d804453284cb0473d5cc5621051a200
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-07-25 01:09:15 +0000

    Avançando no componente de listar tarefas

--- gerenciador-de-tarefas/src/listarTarefas/itensListarTarefas.js
+++ gerenciador-de-tarefas/src/listarTarefas/itensListarTarefas.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { A } from 'hookrouter';
 
 function ItensListarTarefas(props){
 
+    function marcarConcluida(tarefa){
+        return tarefa.concluida ? 'line-through' : 'none';
+    }
+
     return (
-        <h1>Tarefa</h1>
+        props.tarefas.map( tarefa => 
+            <tr key={tarefa.id} data-testid="tarefa">
+                <td width="75%"
+                    data-testid="nome-tarefa"
+                    style={{ textDecoration: marcarConcluida(tarefa) }} >
+                    {tarefa.nome}
+                </td>
+                <td className={"text-center"}>
+                    <A href={"/atualizar/" + tarefa.id }
+                        className={tarefa.concluida ? 'hidden' : 'btn btn-warning btn-sm'}>
+                        <FontAwesomeIcon icon={faEdit} />
+                    </A>
+                </td>
+            </tr>
+        )
     );
 
 }
