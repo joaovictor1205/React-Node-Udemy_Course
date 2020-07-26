08cc3200bc7e30075487412e21681e1432414147
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-07-26 21:19:14 +0000

    Implementando o componente de Remover Tarefa

---
+++ gerenciador-de-tarefas/src/listarTarefas/removerTarefas.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Modal, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
+function RemoverTarefas(props){
+
+    const [exibirModal, setExibirModal] = useState(false);
+
+    function handleAbrirModal(event){
+        event.preventDefault();
+        setExibirModal(true);
+    }
+
+    function handleFecharModal(){
+        setExibirModal(false);
+    }
+
+    return (
+        <span>
+            <Button variant="danger"
+                    className="btn-sm"
+                    onClick={handleAbrirModal}
+                    data-testid="btn-abrir-modal" >
+                <FontAwesomeIcon  icon={faTrashAlt} />
+            </Button>
+            <Modal show={exibirModal} onHide={handleFecharModal} data-testid="modal">
+                <Modal.Header closeButton>
+                    <Modal.Title>Remover tarefa</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    <h3>Deseja deletar a seguinte tarefa?</h3>
+                    <br />
+                    <strong>{props.tarefas.nome}</strong>
+                </Modal.Body>
+
+                <Modal.Footer>
+                    <Button variant="success" onClick={handleRemoverTarefa} data-testid="btn-remover">
+                        Sim
+                    </Button>
+                    <Button variant="danger" onClick={handleFecharModal}>
+                        Não
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </span>
+    );
+}
+
+RemoverTarefas.propTypes = {
+    tarefa: PropTypes.object.isRequired,
+    recarregarTarefas: PropTypes.func.isRequired,
+}
+
+export default RemoverTarefas;
