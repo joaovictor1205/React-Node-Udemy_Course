649ffba83cdefd7757cf0cd932bde99ff0c09e3c
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-07-28 23:03:02 +0000

    Finalizando o componente de paginação

--- gerenciador-de-tarefas/src/listarTarefas/paginacao.js
+++ gerenciador-de-tarefas/src/listarTarefas/paginacao.js
@@ -14,9 +14,61 @@ function Paginacao(props){
         );
     }
 
+    function gerarItemAnterior(){
+        return (
+            <Pagination.Prev
+                key="pagPrev"
+                onClick={() => props.mudarPagina(props.paginaAtual - 1)}
+                disabled={props.paginaAtual === 1}
+            />
+        );
+    }
+
+    function gerarItemNumerico(){
+        return (
+            <Pagination.Item
+                key={pagina}
+                active={pagina === props.paginaAtual}
+                onClick={() => props.mudarPagina(pagina)} >
+            {pagina}
+            </Pagination.Item>
+        );
+    }
+
+    function gerarProximoItem(numPaginas){
+        return(
+            <Pagination.Next 
+                key="pagNext"
+                onClick={() => props.mudarPagina(props.paginaAtual + 1)}
+                disabled={props.paginaAtual === numPaginas}
+            />
+        );
+    }
+
+    function gerarUltimoItem(numPaginas){
+        return(
+            <Pagination.Last 
+                key="pagLast"
+                onClick={() => props.mudarPagina(numPaginas)}
+                disabled={props.paginaAtual === numPaginas}
+            />
+        );
+    }
+
     function obterPaginacao(){
+        const numPaginas = Math.ceil(props.totalItems / props.itemsPorPagina);
         let items =  [];
+
         items.push(gerarPrimeiroItem());
+        items.push(gerarItemAnterior());
+
+        for(let pagina=1; pagina <= numPaginas; pagina++){
+            items.push(gerarItemNumerico(pagina));
+        }
+
+        items.push(gerarProximoItem(numPaginas));
+        items.push(gerarUltimoItem(numPaginas));
+
         return items;
     }
 
