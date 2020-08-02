b8c9a7ff3fb3c30a50d02b8594e6a974e7334593
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-08-02 17:21:06 +0000

    Implementando middleware de rotas

--- quinto-projeto_node-react/serverAPI/app.js
+++ quinto-projeto_node-react/serverAPI/app.js
@@ -8,37 +8,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 ///////////////// ROTAS /////////////////
-
-// Listar todas as tarefas
-app.get('/gerenciador-tarefas', (req, res) => {
-    res.status(501).send({ message: 'Lista tarefas' });
-});
-
-// Listar uma tarefa pelo id
-app.get('/gerenciador-tarefas/:id', (req, res) => {
-    res.send({ message: req.params.id });
-});
-
-// Criar tarefa
-app.post('/gerenciador-tarefas', (req, res) => {
-    res.send({ message: 'Criar tarefa' });
-});
-
-// Atualizar uma tarefa pelo id
-app.put('/gerenciador-tarefas/:id', (req, res) => {
-    res.send({ message: `Atualizar tarefa ${req.params.id}` });
-});
-
-// Deletar uma tarefa pelo id 
-app.delete('/gerenciador-tarefas/:id', (req, res) => {
-    res.send({ message: `Deletar tarefa ${req.params.id}` });
-});
-
-//Concluir uma tarefa pelo id
-app.put('/gerenciador-tarefas/:id/concluir', (req, res) => {
-    res.send({ message: `Concluir a tarefa ${req.params.id}` });
-});
-
+const tarefaRouter = require('./routes/tarefasRoutes');
+app.use('/gerenciador-tarefas', tarefaRouter);
 ////////////// END ROTAS ////////////////
 
 
---
+++ quinto-projeto_node-react/serverAPI/routes/tarefasRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// Listar todas as tarefas
+router.get('/', (req, res) => {
+    res.status(501).send({ message: 'Lista tarefas' });
+});
+
+// Listar uma tarefa pelo id
+router.get('/:id', (req, res) => {
+    res.send({ message: req.params.id });
+});
+
+// Criar tarefa
+router.post('/', (req, res) => {
+    res.send({ message: 'Criar tarefa' });
+});
+
+// Atualizar uma tarefa pelo id
+router.put('/:id', (req, res) => {
+    res.send({ message: `Atualizar tarefa ${req.params.id}` });
+});
+
+// Deletar uma tarefa pelo id 
+router.delete('/:id', (req, res) => {
+    res.send({ message: `Deletar tarefa ${req.params.id}` });
+});
+
+//Concluir uma tarefa pelo id
+router.put('/:id/concluir', (req, res) => {
+    res.send({ message: `Concluir a tarefa ${req.params.id}` });
+});
+
+module.exports = router;
