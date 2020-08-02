64979710da3cc3f8d7553383077f0cfa43bdb814
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-08-02 17:42:16 +0000

    Começando a definir o controller de gerenciamento de tarefas

---
+++ quinto-projeto_node-react/serverAPI/contollers/gerenciadorTarefas.js
@@ -0,0 +1,25 @@
+const uuidv4 = require('uuidv4');
+
+// Armazenar tarefas em memória
+let tarefas = [
+    {
+        id: '1',
+        nome: 'Tarefa 1',
+        concluida: true,
+    },
+    {
+        id: '2',
+        nome: 'Tarefa 2',
+        concluida: false,
+    },
+    {
+        id: '3',
+        nome: 'Tarefa 3',
+        concluida: true,
+    },
+    {
+        id: '4',
+        nome: 'Tarefa 4',
+        concluida: false,
+    },
+];
