b377ebefc258fa73b480559be873fd8cb3e793a7
joaovictor1205 <joaovictor.lol12@gmail.com>
2020-08-02 17:15:16 +0000

    Definindo todas as rotas

--- quinto-projeto_node-react/serverAPI/app.js
+++ quinto-projeto_node-react/serverAPI/app.js
@@ -11,12 +11,32 @@ app.use(cors());
 
 // Listar todas as tarefas
 app.get('/gerenciador-tarefas', (req, res) => {
-    res.status(501).send({ message: 'Não Implementado!'});
+    res.status(501).send({ message: 'Lista tarefas' });
 });
 
 // Listar uma tarefa pelo id
 app.get('/gerenciador-tarefas/:id', (req, res) => {
-    res.send({ message: req.params.id});
+    res.send({ message: req.params.id });
+});
+
+// Criar tarefa
+app.post('/gerenciador-tarefas', (req, res) => {
+    res.send({ message: 'Criar tarefa' });
+});
+
+// Atualizar uma tarefa pelo id
+app.put('/gerenciador-tarefas/:id', (req, res) => {
+    res.send({ message: `Atualizar tarefa ${req.params.id}` });
+});
+
+// Deletar uma tarefa pelo id 
+app.delete('/gerenciador-tarefas/:id', (req, res) => {
+    res.send({ message: `Deletar tarefa ${req.params.id}` });
+});
+
+//Concluir uma tarefa pelo id
+app.put('/gerenciador-tarefas/:id/concluir', (req, res) => {
+    res.send({ message: `Concluir a tarefa ${req.params.id}` });
 });
 
 ////////////// END ROTAS ////////////////
